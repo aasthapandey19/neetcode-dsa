ca726632e3399bb6f589e575d06262104529ee3b
aasthapandey19 <asthaprod99@gmail.com>
2025-06-23 03:34:45 +0530

    *try quickselect method ^

--- arrays-and-hashing/TopKFrequentElements.cpp
+++ arrays-and-hashing/TopKFrequentElements.cpp
@@ -6,7 +6,7 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<vector<int>>sz(nums.size()+1);
         unordered_map<int,int>mp;
-        for(auto x:nums)mp[x]++;
+        for(auto x:nums)mp[x]++; 
         for(auto x:mp){
             sz[x.second].push_back(x.first);
         }
